c31cb881d4fc6b086d0463e064831ed11034c9b9
pozytron <michal.tymula@coderslab.pl>
2021-12-21 22:03:10 +0100

    moved test file to  proper catalog

--- src/sections/__tests__/Roadmap.test.js
+++ src/sections/__tests__/Roadmap.test.js
@@ -1,6 +1,6 @@
 import {render, screen} from '@testing-library/react';
-import {initialData} from "./mocks/roadmap";
-import {Roadmap} from "./sections/Roadmap";
+import {initialData} from "../../mocks/roadmap";
+import {Roadmap} from "../Roadmap";
 import userEvent from "@testing-library/user-event";
 
 describe('Roadmap section testing', () => {

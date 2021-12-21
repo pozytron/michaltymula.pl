de1811cbe9a66c4076a533016c0c1def07fa4dd4
pozytron <michal.tymula@coderslab.pl>
2021-12-21 21:30:53 +0100

    more cleanup, added test, introduced new section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import {initialData} from "./mocks/roadmap";
+import {useState} from "react";
+import {Roadmap} from "./sections/Roadmap";
+
 
 function App() {
+	const [data, setData] = useState(initialData);
+	const handleDataUpdate = () => {
+		setData(initialData.children[0])
+	}
 	return (
 		<div className="App">
 			<header className="App-header">
-					Hello {initialData.name}
+				Hello from pozytron {initialData.name}
 			</header>
+			<Roadmap data={data} onUpdate={handleDataUpdate}/>
 		</div>
 	);
 }
